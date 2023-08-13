000102cf05a1356c8ac1e43a973259c0af2f0d7c
Haziel Ortiz <ya-yo22124@outlook.com>
2023-08-13 17:28:46 -0600

    Creando mi primera practica en PHP

---
+++ calculadora/calculadora.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- links -->
+    <link rel="shortcut icon" href="C:\Users\ya-yo\OneDrive\Documentos\Learning-PHP\img\php-logo.svg" type="image/x-icon">
+    <link rel="stylesheet" href="/styles/styles.css">
+
+    <title>Calculadora PHP</title>
+</head>
+<body>
+    
+</body>
+</html>
---
+++ primera-practica/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="C:\Users\ya-yo\OneDrive\Documentos\Learning-PHP\img\php-logo.svg" type="image/x-icon">
+    <link rel="stylesheet" href="/styles/styles.css">
+    
+    <title>Practicando PHP</title>
+</head>
+<body>
+    <h1>Bienvenido a mi primer práctica de PHP</h1>
+
+    <!-- # Objetivo de la práctica: Crear una página web que muestre un mensaje personalizado utilizando PHP. -->
+
+    <?php
+    // Variable
+    $nombre = "Haziel";
+
+    // Imprimir nombre
+    echo "<p>Hola, mi nombre es $nombre. ¡Estoy aprendiendo PHP!<p/>";
+
+    ?>
+
+
+</body>
+</html>
